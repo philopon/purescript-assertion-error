90c4f67983d642ef29704738215336ffc508a2d5
philopon <philopon.dependence@gmail.com>
2014-12-28 01:21:04 +0900

    reduce indent.

--- examples/Main.purs
+++ examples/Main.purs
@@ -1,33 +1,33 @@
 module Main where
 
-  import Control.Monad.Eff.Exception
-  import Test.Assert.AssertionError
-  import Debug.Trace
-  import Data.Maybe
+import Control.Monad.Eff.Exception
+import Test.Assert.AssertionError
+import Debug.Trace
+import Data.Maybe
 
-  main = do
-    -- create AssertionError
-    let a = assertionError "test" {prop1: 12} Nothing
+main = do
+  -- create AssertionError
+  let a = assertionError "test" {prop1: 12} Nothing
 
-    -- show error
-    trace "======= show error      ======="
-    print a
+  -- show error
+  trace "======= show error      ======="
+  print a
 
-    -- error to JSON with stack trace
-    let j = toJSON true a
-    trace "======= show stack      ======="
-    print j.stack
+  -- error to JSON with stack trace
+  let j = toJSON true a
+  trace "======= show stack      ======="
+  print j.stack
 
-    -- error to JSON String with stack trace
-    let s = toJSONString true a
-    trace "======= show JSON       ======="
-    trace s
+  -- error to JSON String with stack trace
+  let s = toJSONString true a
+  trace "======= show JSON       ======="
+  trace s
 
-    -- convert to Error
-    let e = toError a
-    trace "======= show message    ======="
-    trace (message e)
+  -- convert to Error
+  let e = toError a
+  trace "======= show message    ======="
+  trace (message e)
 
-    -- throw exception
-    trace "======= throw exception ======="
-    throwException e
+  -- throw exception
+  trace "======= throw exception ======="
+  throwException e
--- src/Test/Assert/AssertionError.purs
+++ src/Test/Assert/AssertionError.purs
@@ -6,14 +6,14 @@ module Test.Assert.AssertionError
   , toError
   ) where
 
-  import Control.Monad.Eff.Exception
-  import Test.Assert.AssertionError.Foreign
-  import Data.Maybe
-  import Data.Function
+import Control.Monad.Eff.Exception
+import Test.Assert.AssertionError.Foreign
+import Data.Maybe
+import Data.Function
 
-  foreign import data AssertionError :: *
+foreign import data AssertionError :: *
 
-  foreign import assertionErrorImpl """
+foreign import assertionErrorImpl """
 function assertionErrorImpl(c, msg, props, ssf){
   var ssf = c.fromMaybe(assertionErrorImpl)(ssf);
   return new c.assertionError(msg, props, ssf);
@@ -22,21 +22,21 @@ function assertionErrorImpl(c, msg, props, ssf){
             , assertionError :: assertionError
             } String {|props} (Maybe ssf) AssertionError
 
-  assertionError :: forall props ssf. String -> {|props} -> Maybe ssf -> AssertionError
-  assertionError = runFn4 assertionErrorImpl
-    {fromMaybe: fromMaybe, assertionError: assert}
+assertionError :: forall props ssf. String -> {|props} -> Maybe ssf -> AssertionError
+assertionError = runFn4 assertionErrorImpl
+  {fromMaybe: fromMaybe, assertionError: assert}
 
-  foreign import showAssertionErrorImpl """
+foreign import showAssertionErrorImpl """
 function showAssertionErrorImpl(a){
   return a.toString();
 }""" :: AssertionError -> String
   
-  instance showAssertionError :: Show AssertionError where
-    show = showAssertionErrorImpl
+instance showAssertionError :: Show AssertionError where
+  show = showAssertionErrorImpl
 
-  type AssertionErrorJSON = {name :: String, message :: String, showDiff :: Boolean, stack :: Maybe String}
+type AssertionErrorJSON = {name :: String, message :: String, showDiff :: Boolean, stack :: Maybe String}
 
-  foreign import toJSONImpl """
+foreign import toJSONImpl """
 function toJSONImpl(maybe, with_stack, assetionError){
   var json  = assetionError.toJSON(with_stack);
   var stack = json.stack ? maybe.just(json.stack) : maybe.nothing;
@@ -45,17 +45,17 @@ function toJSONImpl(maybe, with_stack, assetionError){
 }
 """ :: Fn3 {just :: String -> Maybe String, nothing :: Maybe String} Boolean AssertionError AssertionErrorJSON
 
-  toJSON :: Boolean -> AssertionError -> AssertionErrorJSON
-  toJSON = runFn3 toJSONImpl {just: Just, nothing: Nothing}
+toJSON :: Boolean -> AssertionError -> AssertionErrorJSON
+toJSON = runFn3 toJSONImpl {just: Just, nothing: Nothing}
 
-  foreign import toJSONStringImpl """
+foreign import toJSONStringImpl """
 function toJSONStringImpl(with_stack, assetionError){
   return JSON.stringify(assetionError.toJSON(with_stack));
 }""" :: Fn2 Boolean AssertionError String
 
-  toJSONString :: Boolean -> AssertionError -> String
-  toJSONString = runFn2 toJSONStringImpl
+toJSONString :: Boolean -> AssertionError -> String
+toJSONString = runFn2 toJSONStringImpl
 
-  foreign import toError """
+foreign import toError """
 function toError(ae){return ae;}
 """ :: AssertionError -> Error
